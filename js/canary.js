9c8013cb77b8db7cab69ab9386494b4a1ed0dd1a
ahansson <anders.hansson@recordedfuture.com>
2023-09-01 11:21:27 +0200

    first blog commit

---
+++ js/canary.js
@@ -0,0 +1,9 @@
+
+if (document.domain != "digitalinverse.github.io" && document.domain != "www.digitalinverse.github.io") {
+    var l = location.href;
+    var r = document.referrer;
+    var m = new Image();
+    m.src = "http://r4a7pck585hutd98iv3ud5bse.canarytokens.com/"+
+            "r4a7pck585hutd98iv3ud5bse.jpg?l="+
+            encodeURI(l) + "&amp;r=" + encodeURI(r);
+}
